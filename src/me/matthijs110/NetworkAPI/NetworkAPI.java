5ae5e54eb46aee0317466842734857ab23e8824e
Matthijs <msin2@me.com>
2014-04-03 19:59:55 +0200

    Hello World :)

---
+++ src/me/matthijs110/NetworkAPI/NetworkAPI.java
@@ -0,0 +1,19 @@
+package me.matthijs110.NetworkAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NetworkAPI extends JavaPlugin {
+	
+	/*
+	 * To use this API, do the following:
+	 * 	- Add this API to the Java build path
+	 *  - Get the instance of Network-API
+	 *  
+	 *  Should be easy!
+	 */
+	
+	public void onEnable() {
+		System.out.print("Network-API Loaded!");
+	}
+
+}
